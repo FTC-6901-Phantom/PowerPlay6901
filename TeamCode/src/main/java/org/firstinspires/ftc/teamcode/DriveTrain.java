89957363c008eb3f9797178c20c1b919a691794d
andrea <andreangonzalez59@gmail.com>
2022-10-05 21:10:13 -0500

    finished drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -6,15 +6,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp
+@TeleOp (name = "TeleOp69")
 public class DriveTrain extends OpMode {
 
     DcMotor leftFront;
     DcMotor leftBack;
     DcMotor rightFront;
     DcMotor rightBack;
-    Servo gripperLeft;
-    Servo gripperRight;
 
     @Override
     public void init() {
@@ -23,14 +21,12 @@ public class DriveTrain extends OpMode {
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
-        gripperLeft = hardwareMap.get(Servo.class, "gripperLeft");
-        gripperRight = hardwareMap.get(Servo.class, "gripperRight");
+
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
         leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
         leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
-        gripperLeft.setDirection(Servo.Direction.FORWARD);
-        gripperRight.setDirection(Servo.Direction.FORWARD);
+
 
     }
 
@@ -42,14 +38,6 @@ public class DriveTrain extends OpMode {
         leftBack.setPower  (- gamepad1.left_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x);
         rightBack.setPower (- gamepad1.left_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x);
         rightFront.setPower(- gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
-        gripperLeft.setPosition(1);
-        gripperRight.setPosition(1);
-        if (gamepad1.b == true) {
-            gripperLeft.setPosition(1);
-        }
-        if (gamepad1.a == true) {
-            gripperLeft.setPosition(0);
-        }
 
     }
 }
